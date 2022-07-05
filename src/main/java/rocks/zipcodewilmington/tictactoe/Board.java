eef64879016104c7657a9116223f46490c3a8682
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-05 19:38:06 -0400

    board

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,7 +4,16 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    char player1 = 'X';
+    char player2 = 'O';
     public Board(Character[][] matrix) {
+        matrix = new Character[3][3];
+        Character ch = '1';
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++){
+                matrix[i][j] = ch++;
+            }
+        }
     }
 
     public Boolean isInFavorOfX() {
